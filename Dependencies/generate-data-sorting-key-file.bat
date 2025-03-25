d596c928dc578e6b14cb22c1957410153cb75db9
Styro444 <rhomboidal@gmail.com>
2025-03-24 22:43:23 -0500

    Archive
    Archiving old deprecated test files

--- Dependencies/generate-data-sorting-key-file.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "Invoke-RestMethod -Method 'Post' -ContentType 'application/x-www-form-urlencoded' -OutFile P:\order_key.txt -Uri 'https://order.functionalbio.com/admin/generate-data-sorting-key/?auto-download=1' -Body @{ key='5Y0Ho2yZs5S6mTjohw8FE6OLvzg4aB7CThusxxWG77G5cyF6kEOY4m4SmxDECzO' }"
--- Dependencies/get-latest-label-file.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "Invoke-RestMethod -Method 'Post' -ContentType 'application/x-www-form-urlencoded' -OutFile P:\labels.csv -Uri 'https://order.functionalbio.com/admin/label-maker/?auto-download=1' -Body @{ key='LBJyLa9J1JIPzyiRiKyT52fIdnR1Ug8pwb5sLXkgWDzK0QEbOiRThdyOCCJxAw8' }"
