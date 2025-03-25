d5f1613e4a0da5a24d8a7642b5f3ad4951b732b3
Styro444 <rhomboidal@gmail.com>
2025-03-24 22:43:52 -0500

    Deletion
    Deletion of old archived files

--- Old/testingprograms/automation_test.py
+++
@@ -1,6 +0,0 @@
-from pywinauto.application import Application
-
-app = Application(backend='uia').start('notepad.exe').connect(title='Untitled - Notepad',timeout=100)
-#app = Application(backend='uia').connect(title='Untitled - Notepad',timeout=100)
-#app.UntitledNotepad.print_control_identifiers()
-app.UntitledNotepad.child_window(title="Text Editor", auto_id="15", control_type="Edit").wrapper_object()
